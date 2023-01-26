76638e2edbbf9d743f5a19b4b62f9a5200dd3d2b
Mohammad Usman Saud <mohammadusmansaud@gmail.com>
2023-01-26 17:10:02 +0500

    Added Links

--- js/portfolioContact.js
+++ js/portfolioContact.js
@@ -1,21 +1,4 @@
 
-// Map
-// Initialize and add the map
-function initMap() {
-    // The location of Uluru
-    const uluru = { lat: 31.5204, lng: 74.3587 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-    });
-}window.initMap = initMap;
-
 // Scroll top portrait mode
 function myFunctionOrientationSidebar() {
     document.body.scrollTop = 0;
